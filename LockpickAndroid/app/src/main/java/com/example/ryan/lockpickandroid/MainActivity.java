15d4f102e9050e9773004115cbe3b0fc8988a996
Ryan Connors <rjc2872@rit.edu>
2017-04-16 13:22:10 -0400

    Added fumctionality to all operation buttons

--- LockpickAndroid/app/src/main/java/com/example/ryan/lockpickandroid/MainActivity.java
+++ LockpickAndroid/app/src/main/java/com/example/ryan/lockpickandroid/MainActivity.java
@@ -29,21 +29,33 @@ public class MainActivity extends AppCompatActivity {
         status.setText(update);
     }
     public void updatePick2(View v) {
-
+        TextView status = (TextView) findViewById(R.id.StatusView);
+        String update = l.pickPin2();
+        status.setText(update);
     }
     public void updateFeel2(View v) {
-
+        TextView status = (TextView) findViewById(R.id.StatusView);
+        String update = l.feelPin2();
+        status.setText(update);
     }
     public void updatePick3(View v) {
-
+        TextView status = (TextView) findViewById(R.id.StatusView);
+        String update = l.pickPin3();
+        status.setText(update);
     }
     public void updateFeel3(View v) {
-
+        TextView status = (TextView) findViewById(R.id.StatusView);
+        String update = l.feelPin3();
+        status.setText(update);
     }
     public void updatePick4(View v) {
-
+        TextView status = (TextView) findViewById(R.id.StatusView);
+        String update = l.pickPin4();
+        status.setText(update);
     }
     public void updateFeel4(View v) {
-        
+        TextView status = (TextView) findViewById(R.id.StatusView);
+        String update = l.feelPin4();
+        status.setText(update);
     }
 }
