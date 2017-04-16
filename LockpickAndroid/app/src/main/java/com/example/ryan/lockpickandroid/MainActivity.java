33fbe7ff631e399cd5a6c20f9e357857a8769353
Ryan Connors <rjc2872@rit.edu>
2017-04-16 01:36:04 -0400

    Added two buttons with functionality

--- LockpickAndroid/app/src/main/java/com/example/ryan/lockpickandroid/MainActivity.java
+++ LockpickAndroid/app/src/main/java/com/example/ryan/lockpickandroid/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.ryan.lockpickandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static com.example.ryan.lockpickandroid.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Lock l = new Lock(0,0,0,0,false);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void updatePick1(View v) {
+        TextView status = (TextView) findViewById(R.id.StatusView);
+        String update = l.pickPin1();
+        status.setText(update);
+    }
+    public void updateFeel1(View v) {
+        TextView status = (TextView) findViewById(R.id.StatusView);
+        String update = l.feelPin1();
+        status.setText(update);
     }
 }
